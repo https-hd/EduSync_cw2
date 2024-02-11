9c76f5ff09d7053d861b61e2c1ecee7a03bb1c33
Hooda AbdulShakoor <hoodaabdulshakoor@gmail.com>
2024-02-12 02:21:07 +0400

    product.js

---
+++ products.js
@@ -0,0 +1,151 @@
+db.products.insert([
+    {
+        id: 2001,
+        title: "English",
+        description: "Core Subject",
+        price: 300,
+        image: "images/english.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2002,
+        title: "Math",
+        description: "Core Subject",
+        price: 500,
+        image: "images/math.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2003,
+        title: "Sceince",
+        description: "Core Subject",
+        price: 500,
+        image: "images/science.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2004,
+        title: "Language",
+        description: "Core Subject",
+        price: 200,
+        image: "images/languages.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2005,
+        title: "Computer Science",
+        description: "Core Subject",
+        price: 300,
+        image: "images/computer_science.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2006,
+        title: "Board Games",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/board_game.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2007,
+        title: "Art & Craft",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/art_and_craft.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2008,
+        title: "Chess",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/chess.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2009,
+        title: "Baking",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/baking.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2010,
+        title: "Acting",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/acting.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2011,
+        title: "Photography",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/photography.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2012,
+        title: "Designing",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/designing.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2013,
+        title: "Socail Club",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/socializing.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2014,
+        title: "Music",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/music.png",
+        availableInventory: 20,
+        rating: 5
+    },
+
+    {
+        id: 2015,
+        title: "Cooking",
+        description: "Extracurricular activities",
+        price: 300,
+        image: "images/cooking.png",
+        availableInventory: 20,
+        rating: 5
+    }
+])
