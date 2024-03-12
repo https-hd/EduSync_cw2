1a4c0a943db9fb7d903d36cb69d815cd7b45fd36
Hooda AbdulShakoor <hoodaabdulshakoor@gmail.com>
2024-03-13 01:17:48 +0400

    additional

--- server.js
+++ server.js
@@ -27,14 +27,13 @@ MongoClient.connect('mongodb+srv://HA1145:Hooda786@cluster0.bswsi9p.mongodb.net'
     db = client.db('Webstore')
 })
 
-// Logger middleware
+// Logger middleware (from sample code)
 app.use((req, res, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl}`);
-  next();
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl}`);
+    next();
 });
 
-
-// Function to check if a file exists
+// Function to check if a file exists (keeping your original code)
 function fileExists(filePath) {
   return new Promise((resolve) => {
     fs.access(filePath, fs.constants.F_OK, (err) => {
@@ -43,12 +42,11 @@ function fileExists(filePath) {
   });
 }
 
-// Static file middleware for serving images
+// Static file middleware for serving images (keeping your original code)
 app.get('/images/:filename', async (req, res) => {
   const filename = req.params.filename;
   const filePath = path.join(__dirname, 'images', filename);
 
-// Custom middleware for handling non-existent images only when the path starts with "/images"
   try {
       const exists = await fileExists(filePath);
     if (!exists) {
@@ -60,38 +58,29 @@ app.get('/images/:filename', async (req, res) => {
   }
 });
 
-
-//after you type this line, go to terminal and type this command node server.js
 app.get('/', (req, res, next) => {
     res.send('Select a collection, e.g.,/collection/products')
 }) 
 
 app.param('collectionName', (req, res, next, collectionName) => {
     req.collection = db.collection(collectionName)
-    //console.log('collection name', req.collection)
     return next()
 })
 
-//after u write this piece of code, then go to the terminal and type node server.js
-//this is the code to add ur collection basically type http://localhost:3000/collection/lessons into the browser URL, products is the collection name from webstore database in MongoDB Compass software
 app.get('/collection/:collectionName', (req, res, next) => {
     req.collection.find({}).toArray((e, results) => {
-        if (e) return next(e) //e is error
+        if (e) return next(e)
         res.send(results)
     })
 })
 
-//after u write this piece of code, then go to the terminal and type node server.js
 app.post('/collection/:collectionName', (req, res, next) => {
     req.collection.insert(req.body, (e, results) => {
-        if (e) return next(e) //e is error
+        if (e) return next(e)
         res.send(results.ops)
     })
 })
 
-//return with object id
-//this peice of code is to get a product from the database
-//after u write this piece of code, then go to the terminal and type node server.js
 const ObjectID = require('mongodb').ObjectID;
 app.get('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
@@ -100,8 +89,6 @@ app.get('/collection/:collectionName/:id', (req, res, next) => {
     })
 })
 
-//this peice of code is to update anything inside products so eg: price u can just update it using this code
-//after u write this piece of code, then go to the terminal and type node server.js
 app.put('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.update(
         { _id: new ObjectID(req.params.id) },
@@ -114,7 +101,6 @@ app.put('/collection/:collectionName/:id', (req, res, next) => {
     )
 })
 
-//GET request for the search query
 app.get('/collection/:collectionName/search/:k', (req, res) => {
     var key_1 = req.params.k.toLowerCase();
     console.log("Searched term: " + key_1);
@@ -131,84 +117,7 @@ app.get('/collection/:collectionName/search/:k', (req, res) => {
         });
 });
 
-// app.listen(3000, () => {
-//     console.log('Express.js server running at localhost:3000')
-// })
-
 const port = process.env.PORT || 4000
 app.listen(port, () => {
     console.log(`Express.js server running at localhost:${port}`);
 });
-
-// app.get('/collection/:collectionName', (req, res, next) => {
-//     console.log(req.collection)
-//     req.collection.find({}).toArray((e, results) => {
-//         if (e) return next(e)
-//         res.send(results)
-//     })
-// })
-
-
-
-
-// app.post('/collection/:collectionName', (req, res, next) => {
-//     req.collection.insert(req.body, (e, results) => {
-//         if (e) return next(e)
-//         res.send(results.ops)
-//     })
-// })
-
-
-// POST route to update orders
-// app.post('/collection/:collectionName/:id', (req, res, next) => {
-//   const collectionName = req.params.collectionName;
-//   const id = req.params.id;
-//   const updateData = req.body;
-
-//   if (collectionName !== 'orders') {
-//     return res.status(400).send('Invalid collection name');
-//   }
-
-//   // Log the ID and update data for debugging purposes
-//   console.log('Updating order with ID:', id);
-//   console.log('Update data:', updateData);
-
-//   req.collection.updateOne(
-//     { _id: new ObjectID(id) },
-//     { $set: updateData },
-//     { safe: true, upsert: false },
-//     (e, result) => {
-//       if (e) return next(e);
-//       res.send((result.matchedCount >  0) ? { msg: 'success' } : { msg: 'error' });
-//     }
-//   );
-// });
-
-
-// const ObjectID = require('mongodb').ObjectID;
-// app.get('/collection/:collectionName/:id', (req, res, next) => {
-//     req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
-//         if (e) return next(e)
-//         res.send(result)
-//     })
-// })
-
-// app.put('/collection/:collectionName/:id', (req, res, next) => {
-//     req.collection.update(
-//         { _id: new ObjectID(req.params.id) },
-//         { $set: req.body },
-//         { safe: true, multi: false },
-//         (e, result) => {
-//             if (e) return next(e)
-//             res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
-//         })
-// })
-
-// app.delete('/collection/:collectionName/:id', (req, res, next) => {
-//     req.collection.deleteOne(
-//         { _id: new ObjectID(req.params.id) },
-//         (e, result) => {
-//             if (e) return next(e)
-//             res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
-//         })
-// })
